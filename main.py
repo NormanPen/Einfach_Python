ea6221d90b0d38392beeb79e749c7be5e6c31156
Norman Pendzich <73986750+NormanPen@users.noreply.github.com>
2022-09-06 07:23:32 +0200

    lern py1

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+name = "Austin"
+nachname = "Powers"
+alter = 38
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Varianten der formatierten Ausgabe
+print("Hallo " + name, end="")
+print(" Hallo %s %s, du bist %d Jahre alt" % (name, nachname, alter))
+print("Hallo {} {}, fu bist {} Jahre alt ".format(name, nachname, alter))
+print(f"Hallo {name} {nachname}, du bist {alter} Jahre alt")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
